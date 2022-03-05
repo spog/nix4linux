314882041bdc406740a0761396beb3fbb5c7ca47
Samo Pogačnik <samo_pogacnik@t-2.net>
2022-03-05 18:08:29 +0100

    Added installer download and verification.

--- install-multi-user-nix-2.6.1.sh
+++ install-multi-user-nix-2.6.1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+NIX_VERSION="2.6.1"
+INSTALLER_NAME="install-nix-${NIX_VERSION}"
+INSTALLER_URL="https://releases.nixos.org/nix/nix-${NIX_VERSION}/install"
+
 readonly LOGFILE=$(basename $0 .sh).log
 touch $LOGFILE
 exec &> >(tee $LOGFILE)
@@ -15,12 +19,50 @@ if [ "x${INPUT}" != "xy" ]; then
 	echo "Install cancelled!"
 	exit 0
 fi
-set -x
-# Get and verify the installer?
-#curl -o install-nix-2.6.1 https://releases.nixos.org/nix/nix-2.6.1/install
-#curl -o install-nix-2.6.1.asc https://releases.nixos.org/nix/nix-2.6.1/install.asc
-#gpg2 --keyserver hkps://keyserver.ubuntu.com --recv-keys B541D55301270E0BCF15CA5D8170B4726D7198DE
-#gpg2 --verify ./install-nix-2.6.1.asc
 
-sh ./install-nix-2.6.1 --daemon
+# Get and verify the installer?
+if [ ! -f "./${INSTALLER_NAME}" ]; then
+	echo
+	echo "Installer not available!"
+	echo "Remove any signature and download new '${INSTALLER_URL}' to '${INSTALLER_NAME}'"
+	rm -f ./${INSTALLER_NAME}.asc
+	curl -o ./${INSTALLER_NAME} ${INSTALLER_URL}
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo "Error: Installer download failed!"
+		exit $ret
+	fi
+fi
+if [ ! -f "./${INSTALLER_NAME}.asc" ]; then
+	echo
+	echo "Signature not available!"
+	echo "Download new '${INSTALLER_URL}.asc' to '${INSTALLER_NAME}.asc'"
+	curl -o ./${INSTALLER_NAME}.asc ${INSTALLER_URL}.asc
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo "Error: Signature download failed!"
+		exit $ret
+	fi
+	echo
+	echo "Receive GPG keys:"
+	gpg2 --keyserver hkps://keyserver.ubuntu.com --recv-keys B541D55301270E0BCF15CA5D8170B4726D7198DE
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo "Error: Receiveing GPG keys failed!"
+		exit $ret
+	fi
+fi
+
+echo
+echo "Verify the installer:"
+gpg2 --verify ./${INSTALLER_NAME}.asc
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo "Error: Installer verificaion failed!"
+	exit $ret
+fi
+echo
+
+set -x
+sh ./${INSTALLER_NAME} --daemon
 
